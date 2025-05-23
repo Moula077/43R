ac3dde3a1ba89bd967e60ee87e239773d48d0221
Moula077 <dudekulamoulalid2002@gmail.com>
2025-05-23 21:01:19 +0530

    Add files via upload

---
+++ Arithmeticoperators.java
@@ -0,0 +1,25 @@
+package operators;
+
+public class Arithmeticoperators {
+
+	public static void main(String[] args) {
+		
+		int a=30;
+		int b=20;
+		
+		System.out.println("Arithmetic operations");
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println("Relational operations");
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+	}
+}
