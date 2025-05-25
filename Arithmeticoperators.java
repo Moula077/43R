a6f58cc1e36ec2c92028a4195c6a85cc1c182861
Moula077 <dudekulamoulalid2002@gmail.com>
2025-05-25 19:42:41 +0530

    Add files via upload

--- Arithmeticoperators.java
+++ Arithmeticoperators.java
@@ -4,15 +4,21 @@ public class Arithmeticoperators {
 
 	public static void main(String[] args) {
 		
-		int a=30;
-		int b=20;
+		int a=15;
+		int b=4;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=a/b;
+		int g=a%b;
+		
 		
 		System.out.println("Arithmetic operations");
-		System.out.println(a+b);
-		System.out.println(a-b);
-		System.out.println(a*b);
-		System.out.println(a/b);
-		System.out.println(a%b);
+		System.out.println("a+b=" + c);
+		System.out.println("a-b=" + d);
+		System.out.println("a*b=" + e);
+		System.out.println("a/b=" + f);
+		System.out.println("a%b=" + g);
 		
 		System.out.println("Relational operations");
 		System.out.println(a>b);
