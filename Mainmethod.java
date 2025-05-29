85ad74524eae1d9708e8e191dee3d4ed7631c7a9
Moula077 <dudekulamoulalid2002@gmail.com>
2025-05-29 19:11:29 +0530

    Add files via upload

---
+++ Mainmethod.java
@@ -0,0 +1,57 @@
+package com.methods;
+
+public class Mainmethod {
+
+	public static void Addition() {
+		int a=15;
+		int b=25;
+	
+	System.out.println("Addition:" + (a+b));
+	}
+	
+
+	public static void Subtraction() {
+	    int a=35;
+		int b=20;
+		
+	System.out.println("Subtraction:" + (a-b) );
+		}
+
+		public static void Multiplication() {
+			int a=65;
+			int b=25;
+			
+	System.out.println("Multiplication:" + (a*b));
+		}
+
+		public static void Division() {
+				int a=15;
+				int b=5;
+				
+	System.out.println("Division:" + (a/b));
+			}
+	
+		public static void Modulus() {
+			int a=15;
+			int b=10;
+			
+System.out.println("Modulus:" + (a%b));
+		}
+		public static void main(String [] args) {
+			Addition ();
+			Subtraction();
+			Multiplication();
+			Division();
+			Modulus();
+		}
+}
+ 
+
+
+
+
+
+
+
+
+
